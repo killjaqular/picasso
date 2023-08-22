cdcb93e9558fc3d23bab3236dc697d69ae7f7776
killjaqular <adonaypichardo@gmail.com>
2023-08-21 20:28:53 -0700

    added steps to install git, make, docker, docker-compose

--- initDev.sh
+++ initDev.sh
@@ -1,29 +1,38 @@
 # Automate the installation of packages and software needed for this project.
-echo "sudo apt update -y"
 sudo apt update -y
-echo "sudo apt upgrade -y"
 sudo apt upgrade -y
+sudo apt --fix-broken install
+sudo apt autoremove
+sudo apt clean
+sudo apt autoclean
+
+# Install make
+sudo apt install make -y
 
 # Install Golang
-echo "sudo apt install golang -y"
 sudo apt install golang -y
-echo "go version"
 go version
 
+# Install docker compose
+sudo apt install docker.io -y
+
+# Install Docker
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+sudo docker info
+
 # Setup devevlopment environment
-echo '''\
-echo "export GOPATH=$HOME/go" >> ~/.bashrc
-echo "export PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
-source ~/.bashrc
-'''
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
 echo "export PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
 source ~/.bashrc
 
 # Initialize the Go project folder
-echo'''\
-cd server
-go mod init main
-'''
 cd server
 go mod init main
