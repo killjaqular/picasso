5795e4227503b67c81e73c619cd2059b53e937a9
killjaqular <adonaypichardo@gmail.com>
2023-08-22 06:09:01 -0700

    we now connect to the PostgreSQL Docker container

--- server/main.go
+++ server/main.go
@@ -1,64 +1,73 @@
 package main
 
 import (
-	// Standard Golang
-	"database/sql"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+    // Standard Golang
+    "database/sql"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
 
-	// Non-standard Golang
-	"main/lib"
+    // Non-standard Golang
+    "main/lib"
 
-	_ "github.com/lib/pq"
-	"gopkg.in/yaml.v2"
+    _ "github.com/lib/pq"
+    "gopkg.in/yaml.v2"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Picasso")
+    fmt.Fprintln(w, "Picasso")
 }
 
 func main() {
-	////////////////////////////////////////////////////////////////
-	// Initiazlie server
-	////////////////////////////////////////////////////////////////
-	// 1. Open config.yml
-	inputFile, err := ioutil.ReadFile("/picasso/config.yml")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// 2. Get configuration information
-	var config lib.Config
-	err = yaml.Unmarshal(inputFile, &config)
-	if err != nil {
-		log.Fatal(err)
-	}
+    ////////////////////////////////////////////////////////////////
+    // Initialize server
+    ////////////////////////////////////////////////////////////////
+    // 1. Open config.yml
+    inputFile, err := ioutil.ReadFile("/picasso/config.yml")
+    if err != nil {
+        log.Fatal(err)
+    }
+    // 2. Get configuration information
+    var config lib.Config
+    err = yaml.Unmarshal(inputFile, &config)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	fmt.Printf("Server Port: %d\n", config.Server.Port)
-	fmt.Printf("Database Host: %s\n", config.Database.Host)
-	fmt.Printf("Database Port: %d\n", config.Database.Port)
-	fmt.Printf("Database Password: %s\n", config.Database.Database)
-	fmt.Printf("Database Username: %s\n", config.Database.Username)
-	fmt.Printf("Database Password: %s\n", config.Database.Password)
-	port := fmt.Sprintf("%d", config.Server.Port)
-	// 3. Try to connect to the database
-	connStr := "user=YOUR_databaseConnection_USER databaseConnectionname=YOUR_databaseConnection_NAME sslmode=disable"
-	databaseConnection, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer databaseConnection.Close()
+    log.Printf("[i] Server Port: %d\n", config.Server.Port)
+    log.Printf("[i] Database Host: %s\n", config.Database.Host)
+    log.Printf("[i] Database Port: %d\n", config.Database.Port)
+    log.Printf("[i] Database Database: %s\n", config.Database.Database)
+    log.Printf("[i] Database Username: %s\n", config.Database.Username)
+    log.Printf("[i] Database Password: %s\n", config.Database.Password)
+    port := fmt.Sprintf("%d", config.Server.Port)
+    // 3. Try to connect to the database
+    connStr := fmt.Sprintf(
+        "host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+        config.Database.Host,
+        config.Database.Port,
+        config.Database.Username,
+        config.Database.Password,
+        config.Database.Database,
+    )
+    databaseConnection, err := sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer databaseConnection.Close()
 
-	// Ping the database to verify the connection
-	err = databaseConnection.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Connected to PostgreSQL!")
+    // Query the database to verify the connection
+    rows, err := databaseConnection.Query("SELECT 1 FROM uuids.uuid;")
+    if err != nil {
+        log.Fatal(err)
+    }else{
+        log.Printf("[i] Connected to %s:%d", config.Database.Host, config.Database.Port)
+    }
+    defer rows.Close()
 
-	// 4. Try to host service on host:port
-	http.HandleFunc("/", homePage)
-	log.Printf("Picasso server on port %s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+    // 4. Try to host service on host:port
+    http.HandleFunc("/", homePage)
+    log.Printf("[i] Picasso server on port %s\n", port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
