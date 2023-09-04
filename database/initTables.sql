9b524e33f5bba2a42161efb15886596aa909adf4
Adonay Pichardo <adonaypichardo@gmail.com>
2023-09-04 10:23:12 -0700

    rename and updated sessionKey table

---
+++ database/initTables.sql
@@ -0,0 +1,31 @@
+SET TIME ZONE 'UTC';
+
+CREATE SCHEMA IF NOT EXISTS accounts;
+CREATE SCHEMA IF NOT EXISTS images;
+CREATE SCHEMA IF NOT EXISTS sessionKeys;
+CREATE SCHEMA IF NOT EXISTS uuids;
+
+CREATE TABLE IF NOT EXISTS uuids.uuid (
+    uuid UUID PRIMARY KEY,
+    parentTable VARCHAR(64) -- The table that contains the actual asset of the UUID
+);
+
+CREATE TABLE IF NOT EXISTS accounts.user (
+    uuid UUID PRIMARY KEY REFERENCES uuids.uuid(uuid),
+    username VARCHAR(16) UNIQUE, -- No repeated usernames allowed
+    password VARCHAR(256) -- SHA256
+);
+
+-- It is recommended images be stored on local file disk versus database
+-- We will store metadata here, like the path on the local file system
+CREATE TABLE IF NOT EXISTS images.image (
+    uuid UUID PRIMARY KEY REFERENCES uuids.uuid(uuid),
+    filePath VARCHAR(255), -- Maximum directory length in the ext4 file system, common in Ubuntu
+    userUUID UUID REFERENCES accounts.user(uuid) -- Foreign key referencing user UUID
+);
+
+CREATE TABLE IF NOT EXISTS sessionKeys.session (
+    sessionKey BYTEA, -- The session key of a user
+    userUUID UUID REFERENCES accounts.user(uuid), -- Foreign key referencing user UUID
+    expiration TIMESTAMP WITH TIME ZONE -- Time sessionKey will expire and user must re-login
+);
